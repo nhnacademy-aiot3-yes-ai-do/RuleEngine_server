f474461f70e20e79b77b321e9d80f2c74e03fbf8
Poly-Etilen <wlsdud04060@naver.com>
2026-07-15 13:04:43 +0900

    first commit

---
+++ src/main/java/site/yesaido/ruleengine_server/RuleEngineServerApplication.java
@@ -0,0 +1,13 @@
+package site.yesaido.ruleengine_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuleEngineServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/yesaido/ruleengine_server/RuleEngineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.yesaido.ruleengine_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
